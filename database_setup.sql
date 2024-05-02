cd02001a164abb7c78bccc776d20a8add5c22bd5
AbrahamledrickRodriguez <97562328+AbrahamledrickRodriguez@users.noreply.github.com>
2024-05-02 18:46:03 -0400

    Add files via upload

---
+++ database_setup.sql
@@ -0,0 +1,51 @@
+create database if not exists meal_planner;
+
+use meal_planner;
+
+create table if not exists user (
+    u_id int auto_increment,
+    username varchar(255) not null,
+    password varchar(255) not null,
+    primary key (u_id),
+    unique (username)
+);
+
+create table if not exists food (
+    f_id int auto_increment,
+    f_name varchar(255) not null,
+    calories int not null,
+    primary key (f_id)
+);
+
+
+create table if not exists dininghall (
+    dh_id int auto_increment,
+    dh_name varchar(255) not null,
+    primary key (dh_id)
+);
+
+CREATE TABLE IF NOT EXISTS MealPlan (
+    m_id INT AUTO_INCREMENT,
+    m_name VARCHAR(255) NOT NULL,
+    u_id INT NOT NULL,
+    dh_id INT NOT NULL,
+    PRIMARY KEY (m_id),
+    FOREIGN KEY (u_id) REFERENCES User(u_id),
+    FOREIGN KEY (dh_id) REFERENCES DiningHall(dh_id)
+);
+
+CREATE TABLE IF NOT EXISTS FoodInMealPlan (
+    m_id INT NOT NULL,
+    f_id INT NOT NULL,
+    PRIMARY KEY (m_id, f_id),
+    FOREIGN KEY (m_id) REFERENCES MealPlan(m_id),
+    FOREIGN KEY (f_id) REFERENCES Food(f_id)
+);
+
+CREATE TABLE IF NOT EXISTS FoodInDiningHall (
+    dh_id INT NOT NULL,
+    f_id INT NOT NULL,
+    PRIMARY KEY (dh_id, f_id),
+    FOREIGN KEY (dh_id) REFERENCES DiningHall(dh_id),
+    FOREIGN KEY (f_id) REFERENCES Food(f_id)
+);

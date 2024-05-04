371d0b807f9e7df84f74e97d43ebf171593be487
dylanapollock <82855481+dylanapollock@users.noreply.github.com>
2024-05-04 19:37:18 -0400

    Update database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -50,8 +50,6 @@ CREATE TABLE IF NOT EXISTS FoodInDiningHall (
     FOREIGN KEY (f_id) REFERENCES Food(f_id)
 );
 
--- See readme for instructions on how to import the food table values.
-
 insert into dininghall(dh_name) values ("Bolton");
 
 insert into dininghall(dh_name) values ("Snelling");
@@ -61,5 +59,6 @@ insert into dininghall(dh_name) values ("O-House");
 insert into dininghall(dh_name) values ("Joe Frank");
 
 insert into dininghall(dh_name) values ("The Niche");
-    
+
+-- See readme for instructions on how to import the food and FoodInDiningHall table values.
 
